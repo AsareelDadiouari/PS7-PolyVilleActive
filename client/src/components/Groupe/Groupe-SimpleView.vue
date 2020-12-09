99eeb8fe9d89d261e071da960ae5ea0c12e5f454
Asaréel <asareel56@gmail.com>
2020-12-09 18:13:13 +0100

    #18 Component update

--- client/src/components/Groupe/Groupe-SimpleView.vue
+++ client/src/components/Groupe/Groupe-SimpleView.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="groupe">
     <div class="groupe-desc">
-      <div class="title is-6"> Nom du groupe </div> <!--{{groupe.nom}}-->
-      <div class="subtitle is-7"> 900 membres - 1 publication par jour</div> <!--{{groupe.membres}} - {{groupe.publications}}-->
+      <div class="title is-6"> Nom du groupe</div> <!--{{groupe.nom}}-->
+      <div class="subtitle is-7"> 900 membres - 1 publication par jour</div>
+      <!--{{groupe.membres}} - {{groupe.publications}}-->
     </div>
   </div>
 </template>
