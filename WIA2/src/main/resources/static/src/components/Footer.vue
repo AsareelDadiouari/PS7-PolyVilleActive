f67769031347dff2799a8fdf4e7e05b8d874aa93
Lucie <lucie.morant@etu.univ-cotedazur.fr>
2020-12-10 15:26:03 +0100

    #18 Ajouts de router au Footer
    #18 Ajouts de router au Footer

--- WIA2/src/main/resources/static/src/components/Footer.vue
+++ WIA2/src/main/resources/static/src/components/Footer.vue
@@ -6,16 +6,20 @@
           <div class="column is-3 is-offset-2">
             <h2><strong>Tourisme</strong></h2>
             <ul>
-              <li><a href="#">Patrimoine</a></li>
-              <li><a href="#">Centres sportifs</a></li>
-              <li><a href="#">Commerces</a></li>
+              <li><router-link to="/patrimoine">
+              <a href="#">Patrimoine</a></router-link></li>
+              <li><router-link to="/centres-sportifs">
+              <a href="#">Centres sportifs</a></router-link></li>
+              <li><router-link to="/commerces">
+              <a href="#">Commerces</a></router-link></li>
               <li><a href="#">Présentation et accès</a></li>
             </ul>
           </div>
           <div class="column is-3">
             <h2><strong>Services</strong></h2>
             <ul>
-              <li><a href="#">Événements</a></li>
+              <li><router-link to="/evenements">
+              <a href="#">Événements</a></router-link></li>
               <li><a href="#">Stationement et transport en commun</a></li>
             </ul>
           </div>
