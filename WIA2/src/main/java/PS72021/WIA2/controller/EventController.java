5b8b868ff6fadb899efde668431b48b7a37b1028
Elise Chamberlin <elise.chamberlin@etu.unice.fr>
2020-12-15 09:44:43 +0100

    #20 ajout des évenements

--- WIA2/src/main/java/PS72021/WIA2/controller/EventController.java
+++ WIA2/src/main/java/PS72021/WIA2/controller/EventController.java
@@ -3,8 +3,6 @@ package PS72021.WIA2.controller;
 import PS72021.WIA2.Application;
 import PS72021.WIA2.model.Event;
 import PS72021.WIA2.model.User;
-import org.apache.jena.base.Sys;
-import org.apache.jena.dboe.transaction.txn.SysTransState;
 import org.apache.jena.query.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +21,12 @@ public class EventController {
         String query = "SELECT DISTINCT ?s ?p ?o WHERE {\n" +
                 "?s <http://www.ps7-wia2.com/events#events> ?o " +
                 "}";
-        String query2;
 
-        String filePath = "database/evenements.jsonld";
+        String filePath = "database/evenements2.jsonld";
         ResultSet results = Application.executeQuery(query, filePath);
         for (int i = 1; results.hasNext(); i++) {
             QuerySolution querySolution = results.next();
-            query = "SELECT * WHERE {\n" +
+           query = "SELECT * WHERE {\n" +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#name_fr> ?name_fr." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#start> ?start." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#end> ?end." +
@@ -37,10 +34,10 @@ public class EventController {
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#profile> ?profile." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#categories> ?categories." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#description> ?description." +
-                    "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#image> ?image." +
+                    "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#images> ?images." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#latitude> ?latitude." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#longitude> ?longitude." +
-                    "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#users> ?users." +
+                   /* "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#users> ?users." +*/
                     "}";
             ResultSet results2 = Application.executeQuery(query, filePath);
             QuerySolution sol = results2.next();
@@ -56,28 +53,28 @@ public class EventController {
             }
 
             ArrayList<String> categories = new ArrayList<>();
-            categories.add(sol.get("categories").toString());
+           categories.add(sol.get("categories").toString());
             for (;results2.hasNext();) {
                 sol = results2.next();
                 categories.add(sol.get("categories").toString());
             }
-
-            query2 = "SELECT ?users WHERE {" +
+            ArrayList<String> users = new ArrayList<>();
+           /*String query2 = "SELECT ?users WHERE {" +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#users> ?users." +
                     "}";
             ResultSet resultSet3 = Application.executeQuery(query2, filePath);
             QuerySolution soln = resultSet3.nextSolution();
 
-            ArrayList<String> users = new ArrayList<>();
+
             users.add(soln.get("users").toString());
             for (;resultSet3.hasNext();){
                 String temp = resultSet3.next().get("users").toString();
                 users.add(temp);
-            }
+            }*/
 
             String[] test = querySolution.get("o").toString().split("/", -1);
             Event event = new Event (Integer.parseInt(test[test.length-1]), sol.get("?name_fr").toString(), start, end, sol.get("address").toString(),profiles,categories,
-                    sol.get("description").toString(),sol.get("image").toString(),sol.get("latitude").asLiteral().getDouble(),
+                    sol.get("description").toString(),sol.get("images").toString(),sol.get("latitude").asLiteral().getDouble(),
                     sol.get("longitude").asLiteral().getDouble(), users);
             events.add(event);
         }
@@ -94,7 +91,7 @@ public class EventController {
                 "?s <http://www.ps7-wia2.com/events#events> ?o " +
                 "}";
 
-        String filePath = "database/evenements.jsonld";
+        String filePath = "database/evenements2.jsonld";
         ResultSet results = Application.executeQuery(query, filePath);
         for (int i = 1; results.hasNext(); i++) {
             QuerySolution querySolution = results.next();
@@ -106,10 +103,10 @@ public class EventController {
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#profile> ?profile." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#categories> ?categories." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#description> ?description." +
-                    "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#image> ?image." +
+                    "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#images> ?images." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#latitude> ?latitude." +
                     "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#longitude> ?longitude." +
-                    "<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#users> ?users." +
+                    /*"<" + querySolution.get("o") + "> <http://www.ps7-wia2.com/events#users> ?users." +*/
                     "}";
             ResultSet results2 = Application.executeQuery(query, filePath);
             QuerySolution sol = results2.next();
@@ -134,11 +131,11 @@ public class EventController {
 
             if(recommanded) {
                 ArrayList<String> usersIndb = new ArrayList<>();
-                usersIndb.add(sol.get("users").toString());
+                /*usersIndb.add(sol.get("users").toString());
                 for (;results2.hasNext();) {
                     sol = results2.next();
                     usersIndb.add(sol.get("users").toString().substring(sol.get("users").toString().length() - 1));
-                }
+                }*/
 
                 LocalDate start = LocalDate.parse(sol.get("?start").toString());
                 LocalDate end = LocalDate.parse(sol.get("?end").toString());
@@ -151,7 +148,7 @@ public class EventController {
 
                 String[] test = querySolution.get("o").toString().split("/", -1);
                 Event event = new Event(Integer.parseInt(test[test.length - 1]), sol.get("?name_fr").toString(), start, end, sol.get("address").toString(),
-                        profiles, categories, sol.get("description").toString(), sol.get("image").toString(), sol.get("latitude").asLiteral().getDouble(),
+                        profiles, categories, sol.get("description").toString(), sol.get("images").toString(), sol.get("latitude").asLiteral().getDouble(),
                         sol.get("longitude").asLiteral().getDouble(), usersIndb);
                 events.add(event);
             }
